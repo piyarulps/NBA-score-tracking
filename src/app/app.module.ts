3d399f09e2554de631691781abafa0342bbf7777
piyarul sk <piyarul.pss@gmail.com>
2023-03-05 02:23:03 +0530

    update app similar to stackblitz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AccordionModule} from 'primeng/accordion';
-import {DropdownModule} from 'primeng/dropdown';
-import {FormsModule} from '@angular/forms';
-import {ButtonModule} from 'primeng/button';
-import {  HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AccordionModule } from 'primeng/accordion';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './api.interceptor';
 import { ApiService } from './api.service';
-import {CardModule} from 'primeng/card';
+import { CardModule } from 'primeng/card';
 import { NbaScoreComponent } from './nba-score/nba-score.component';
 import { NbaTeamResultComponent } from './nba-team-result/nba-team-result.component';
 import { SocreboardPipe } from './Shared/pipe/socreboard.pipe';
+import { BackButtonComponent } from './Shared/components/back-button/back-button.component';
+import {SkeletonModule} from 'primeng/skeleton';
 
 
 @NgModule({
@@ -22,7 +24,8 @@ import { SocreboardPipe } from './Shared/pipe/socreboard.pipe';
     AppComponent,
     NbaScoreComponent,
     NbaTeamResultComponent,
-    SocreboardPipe
+    SocreboardPipe,
+    BackButtonComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,15 +36,17 @@ import { SocreboardPipe } from './Shared/pipe/socreboard.pipe';
     FormsModule,
     ButtonModule,
     HttpClientModule,
-    CardModule
+    CardModule,
+    SkeletonModule
   ],
   providers: [
-    
-    ApiService, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptor,
-    multi: true }
-],
-  bootstrap: [AppComponent]
+    ApiService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/nba-score/nba-score.component.ts
+++ src/app/nba-score/nba-score.component.ts
@@ -7,87 +7,88 @@ import { teamdetails, TeamList, TeamListDeatils } from '../Shared/team.modal';
 @Component({
   selector: 'app-nba-score',
   templateUrl: './nba-score.component.html',
-  styleUrls: ['./nba-score.component.scss']
+  styleUrls: ['./nba-score.component.scss'],
 })
-export class NbaScoreComponent implements OnInit{
-  public teams: TeamList[] =[];
+export class NbaScoreComponent implements OnInit {
+  public teams: TeamList[] = [];
   public selectedTeam!: TeamList;
-  public seelectedAllTeam:Array<TeamListDeatils>=[]
-  public teamdetails:Array<teamdetails> =[];
-  public ImgURl:string=GlobalConst.imgURL;
-  public fileType:string =GlobalConst.fileType;
-  constructor(private service:ApiService , private router:Router){
+  public seelectedAllTeam: Array<TeamListDeatils> = [];
+  public teamdetails: Array<teamdetails> = [];
+  public ImgURl: string = GlobalConst.imgURL;
+  public fileType: string = GlobalConst.fileType;
+  constructor(private service: ApiService, private router: Router) {
     this.getData();
   }
   ngOnInit(): void {
-    this.seelectedAllTeam =this.service.selectedTeamList; 
+    this.seelectedAllTeam = this.service.selectedTeamList;
   }
 
-  trackTeam(){
-    const dateParams=this.service.getParams();
-    const treakTeamId=this.selectedTeam.id;
-    const uniqueCheck:boolean =this.checkAvailability(this.seelectedAllTeam,treakTeamId);
-    if(!uniqueCheck){
-      this.service.getTeam(treakTeamId,dateParams).subscribe(res=>{
-        const fetchData=this.scoreBoardData(res.data,treakTeamId);
-        const teamData:any=this.selectedTeam;
-        teamData.results=fetchData.results;
-        teamData.selfAvgScore= (fetchData.selfAvgScore/fetchData.results.length).toFixed(2);
-        teamData.opptAvgScore=(fetchData.opptAvgScore/fetchData.results.length).toFixed(2);;
+  trackTeam() {
+    const dateParams = this.service.getParams();
+    const treakTeamId = this.selectedTeam.id;
+    const uniqueCheck: boolean = this.checkAvailability(
+      this.seelectedAllTeam,
+      treakTeamId
+    );
+    if (!uniqueCheck) {
+       this.service.getTeam(treakTeamId, dateParams).subscribe((res) => {
+        const fetchData = this.scoreBoardData(res.data, treakTeamId);
+        const teamData: any = this.selectedTeam;
+        teamData.results = fetchData.results;
+        teamData.selfAvgScore = (
+          fetchData.selfAvgScore / fetchData.results.length
+        ).toFixed(2);
+        teamData.opptAvgScore = (
+          fetchData.opptAvgScore / fetchData.results.length
+        ).toFixed(2);
         this.seelectedAllTeam.push(teamData);
-        this.service.selectedTeamList= this.seelectedAllTeam;
-      })
+        this.service.selectedTeamList = this.seelectedAllTeam;
+      });
     }
   }
- 
-   private checkAvailability(arr:Array<TeamListDeatils>, val:number) {
-      return arr.some((arrVal) => val === arrVal.id);
-    }
-    
 
+  private checkAvailability(arr: Array<TeamListDeatils>, val: number) {
+    return arr.some((arrVal) => val === arrVal.id);
+  }
 
-  private scoreBoardData(fetchData: any, selectedId:number) {
-    let totalSelfScore=0;
-    let totalOpptScore=0;
+  private scoreBoardData(fetchData: any, selectedId: number) {
+    let totalSelfScore = 0;
+    let totalOpptScore = 0;
     fetchData.map((element: any) => {
       if (element.home_team.id == selectedId) {
         element['self_score'] = element.home_team_score;
         element['selfTeam'] = element.home_team.abbreviation;
         element['oppt_score'] = element.visitor_team_score;
         element['OpptTeam'] = element.visitor_team.abbreviation;
-        totalSelfScore=totalSelfScore+element.home_team_score;
-        totalOpptScore=totalOpptScore +element.visitor_team_score;
-      }
-      else if (element.visitor_team.id == selectedId) {
+        totalSelfScore = totalSelfScore + element.home_team_score;
+        totalOpptScore = totalOpptScore + element.visitor_team_score;
+      } else if (element.visitor_team.id == selectedId) {
         element['self_score'] = element.visitor_team_score;
         element['selfTeam'] = element.visitor_team.abbreviation;
         element['oppt_score'] = element.home_team_score;
         element['OpptTeam'] = element.home_team.abbreviation;
-        totalSelfScore=totalSelfScore+element.visitor_team_score;
-        totalOpptScore=totalOpptScore +element.home_team_score;
+        totalSelfScore = totalSelfScore + element.visitor_team_score;
+        totalOpptScore = totalOpptScore + element.home_team_score;
       }
-    });    
-    const retrunData={
-      selfAvgScore:totalSelfScore,
-      opptAvgScore:totalOpptScore,
-      results:fetchData,
-    }
-    return retrunData
+    });
+    const retrunData = {
+      selfAvgScore: totalSelfScore,
+      opptAvgScore: totalOpptScore,
+      results: fetchData,
+    };
+    return retrunData;
   }
 
   private getData() {
-    this.service.getTeamList().subscribe(res=>{
-      this.teams=res.data;
-    })
+    this.service.getTeamList().subscribe((res) => {
+      this.teams = res.data;
+    });
   }
-  closeTeam(index:number){
+  closeTeam(index: number) {
     this.seelectedAllTeam.splice(index, 1);
-    this.service.selectedTeamList= this.seelectedAllTeam;
-
-    
+    this.service.selectedTeamList = this.seelectedAllTeam;
   }
-  resultPage(teamCode:any){
+  resultPage(teamCode: any) {
     this.router.navigate(['/results', teamCode.id]);
-
   }
 }
